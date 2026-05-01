5af735031cf7f57df685bee992ecfce0e19dc6bd
onlinesupergamer <bennyguybills@gmail.com>
2026-04-30 20:29:31 -0600

    Car Script Added

---
+++ Source/Car/NewScript.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class NewScript : RigidBody3D
+{
+	
+	public override void _Ready()
+	{
+	}
+
+	
+	public override void _Process(double delta)
+	{
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+}
