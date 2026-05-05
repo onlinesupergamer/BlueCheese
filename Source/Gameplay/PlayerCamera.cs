9e9b0e37e4a3d13cb010d82c0cd19af6b8c30c8d
onlinesupergamer <bennyguybills@gmail.com>
2026-05-04 22:33:31 -0600

    Bug Fixes

--- Source/Gameplay/PlayerCamera.cs
+++ Source/Gameplay/PlayerCamera.cs
@@ -23,8 +23,8 @@ public partial class PlayerCamera : Camera3D
         Vector3 TargetPos = m_Car.GlobalPosition + m_Car.Basis.Z * DistanceOffset + m_Car.Basis.Y * HeightOffset;
 
 
-		GlobalPosition = GlobalPosition.Lerp(TargetPos, (float)delta * 10.0f);
-		GlobalBasis = GlobalBasis.Slerp(m_Car.Basis, (float)delta * 4.0f);
+		GlobalPosition = GlobalPosition.Lerp(TargetPos, (float)delta * 20.0f);
+		GlobalBasis = GlobalBasis.Slerp(m_Car.Basis, (float)delta * 8.0f);
     }
 
 }
